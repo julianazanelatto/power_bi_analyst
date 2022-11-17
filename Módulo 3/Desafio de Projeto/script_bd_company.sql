a8010f14090d88603787ed8e45ff69690e64db66
julianazanelatto <julianazanelatto@gmail.com>
2022-11-17 00:30:25 -0300

    update

--- Módulo 3/Desafio de Projeto/script_bd_company.sql
+++ Módulo 3/Desafio de Projeto/script_bd_company.sql
@@ -1,8 +1,8 @@
-create schema if not exists company_constraints;
-use company_constraints;
+create schema if not exists azure_company;
+use azure_company;
 
 select * from information_schema.table_constraints
-	where constraint_schema = 'company_constraints';
+	where constraint_schema = 'azure_company';
 
 -- restrição atribuida a um domínio
 -- create domain D_num as int check(D_num> 0 and D_num< 21);
